b3f84a7bf4508059fcdf821eae72ab6f301458dc
Raquel Bujans <raquel.bujans@gmail.com>
2017-07-30 17:05:30 -0400

    include dist/

---
+++ dist/scripts/requestTest.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var WebPageTest = require('WebPageTest');
+var wpt = new WebPageTest('https://www.webpagetest.org/', process.env.WPT_KEY_API || 'A.5d516bf1ca465c213f41787574d5bb26');
+
+// console.log('Running test');
+wpt.runTest('https://www.compass.com/search/sales/nyc/', {
+  connectivity: 'Cable',
+  location: 'Dulles:Chrome',
+  firstViewOnly: false,
+  runs: 1,
+  video: true,
+  dryRun: true,
+  pingback: 'https://kpvvlghmsx.localtunnel.me/webpagetest'
+  // pollResults: 5,
+  // dryRun: 'true',
+}, function (err, result) {
+  console.log(err || result);
+});
---
+++ dist/scripts/runHelper.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var helpers = require('./helpers');
+helpers.lookUpTestResults('170730_04_15P7');
+console.log(helpers);
