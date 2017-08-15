48c34ee7fbb7fab6e5111849d71862b40be5d9a1
Raquel Bujans <raquel.bujans@gmail.com>
2017-08-14 22:42:12 -0400

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,11 @@ router.get('/webpagetest', function(req, res, next) {
   helpers.fetchTestResults(req.query.id, req, res);
 });
 
+// TODO: take URL as param
 router.get('/charts', function(req, res, next) {
-  const cursor = req.db.collection('results').find();
+  const cursor = req.db.collection('results').find({
+    "url": "https://www.compass.com/search/sales/nyc/"
+  });
   cursor.toArray().then((records) => {
     res.send(records);
   });
